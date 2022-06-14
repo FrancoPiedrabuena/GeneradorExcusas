93e5f309dbad270216c18f55395bf671ef87366c
FrancoPiedrabuena <franconpiedrabuena@gmail.com>
2022-06-14 19:33:45 +0000

    Mi primer generador

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = function() {
+  let who = ["the dog", "my grandma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return `${who[whoIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+};
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
